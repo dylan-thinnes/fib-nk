8d7b101952d003b69aa23ca37a909445724232aa
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-07-27 13:00:50 +0100

    Clean up Haskell solution
    - Make the Group structure hold k,n metadata
    - Move debugPrinting to debugGroup
    - Add some commenting

--- golden.hs
+++ golden.hs
@@ -1,4 +1,5 @@
 import Prelude hiding (cycle)
+import qualified Prelude (cycle)
 import qualified Data.Set as S
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
@@ -9,7 +10,10 @@ import System.Environment
 type Seed = [Int]
 type Step = Seed -> Seed
 newtype Cycle = Cycle { uncycle :: [Seed] }
-newtype Group = Group { ungroup :: [Cycle] }
+data Group = Group { k :: Int
+                   , n :: Int
+                   , ungroup :: [Cycle]
+                   }
 type Domain = S.Set Seed
 
 main = do
@@ -17,10 +21,7 @@ main = do
     let k = read arg0
     let n = read arg1
     let group = fibnk n k
-    putStr   $ show group
-    putStrLn $ show (length $ ungroup group) 
-            ++ " cycles found for k = " ++ show k 
-            ++ ", n = " ++ show n
+    putStr $ debugGroup group
 
 -- FIB-SPECIFIC
 
@@ -38,7 +39,7 @@ domain n k = S.fromList $ f n k
 
 -- Finds all cycles for a specific fibonacci progression
 fibnk :: Int -> Int -> Group
-fibnk n k = cycles (incrMod n) $ domain n k
+fibnk n k = Group k n $ cycles (incrMod n) $ domain n k
 
 -- GENERAL
 
@@ -48,8 +49,8 @@ cycle step seed = Cycle $ seed : takeWhile (/= seed) (iterate step (step seed))
 
 -- Find all the cycles of an injective function on a finite domain, given a set
 -- for the domain
-cycles :: Step -> Domain -> Group
-cycles step domain = Group $ f domain []
+cycles :: Step -> Domain -> [Cycle]
+cycles step domain = f domain []
     where
     f :: Domain -> [Cycle] -> [Cycle]
     f domain found 
@@ -59,21 +60,36 @@ cycles step domain = Group $ f domain []
       = let newCycle = cycle step (S.findMin domain)
          in f (foldr S.delete domain $ uncycle newCycle) (newCycle : found)
 
+-- Pretty printing of cycles and groups
+
 instance Show Cycle where
     show (Cycle c) = unwords $ map show $ reverse (head c) ++ map head (tail c)
 
 instance Show Group where
-    show (Group cycles) = unlines $ map f $ zip [0..] cycles
+    show (Group _ _ cycles) = unlines $ map f $ zip [0..] cycles
         where
         f :: (Int, Cycle) -> String
         f (n,c) = "Cycle #" ++ show n ++ ":\n " ++ show c
                   ++ "\n  length: " ++ show (length $ uncycle c)
 
 cycleLengths :: Group -> M.Map Int Int
-cycleLengths (Group cycles) = foldr f M.empty cycles
+cycleLengths (Group _ _ cycles) = foldr f M.empty cycles
     where
     f = M.alter g . length . uncycle
     g = Just . (+1) . fromMaybe 0
+
+-- Show a group in the simplest way possible, one line per cycle, one word per
+-- element
+-- Make sure that the end is padded w/ values from the beginning, so that all
+-- states can be reconstructed in a single pass
+debugGroup :: Group -> String
+debugGroup (Group n k cycles) = unlines $ map debugCycle $ cycles
+    where
+    debugCycle cycle 
+      = let states = uncycle cycle
+            values = map head states
+         in unwords $ map show 
+                    $ values ++ take (n-1) (Prelude.cycle values)
 
 -- Aggregate reports
 
