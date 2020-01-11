923a332411fbf26fe8a398c3791a22abb1b9a68d
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-01-11 16:30:13 +0000

    Fix golden.clj to report full final seed

--- golden.clj
+++ golden.clj
@@ -1,9 +1,6 @@
 (require 'clojure.string)
 
-(def k (Integer/parseInt (first  *command-line-args*)))
-(def n (Integer/parseInt (second *command-line-args*)))
-
-(defn next_seed [seed n k]
+(defn next_seed [seed k n]
   (+
     (mod (* seed n) (int (Math/floor (Math/pow n k))))
     (loop [seed seed acc 0]
@@ -11,28 +8,31 @@
           (mod acc n)
           (recur (quot seed n) (+ acc (mod seed n)))))))
 
-(defn cycles [n k]
-  (reduce #(assoc %1 %2 (next_seed %2 n k)) {} (range (Math/pow n k))))
+(defn cycles [k n]
+  (reduce #(assoc %1 %2 (next_seed %2 k n)) {} (range (Math/pow n k))))
 
 (defn unspool [m orig]
-  (loop [k orig acc '()]
+  (loop [k orig acc []]
     (if (or
           (and (= k orig) (not (empty? acc)))
           (not (contains? m k)))
-        (reverse acc)
+        acc
         (recur (get m k) (conj acc k)))))
 
 (defn unspool-all [m]
-  (loop [m m acc '()]
+  (loop [m m acc []]
     (if (empty? m)
         acc
         (let [spool (unspool m (first (keys m)))]
           (recur (apply dissoc m spool) (conj acc spool))))))
 
+(def k (Integer/parseInt (first  *command-line-args*)))
+(def n (Integer/parseInt (second *command-line-args*)))
+
 (doseq
-  [spool (unspool-all (cycles n k))]
+  [spool (unspool-all (cycles k n))]
   (println
     (clojure.string/join
       " "
-      (map #(mod % n)
-        (conj spool (last spool))))))
+      (mapv #(mod % n)
+        (apply conj spool (take (- n 1) spool))))))
